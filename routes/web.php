25c4cb05cdc8563b190c2b4c824b4b6192bb2ffa
YasinZhang <271155397@qq.com>
2018-01-28 23:04:04 +0800

    fix error

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', 'StaticPageController@home');
+Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
